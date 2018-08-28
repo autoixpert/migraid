d0e3ab0610b3c22b5d6a1e042035717e5bd99bf0
Steffen Langer <sl@autoixpert.de>
2018-08-28 09:16:22 +0200

    Prevent accidentally adding BETA migration scripts to development or production.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const migraid     = require('commander');
 const packageJson = require('./package');
 const Migrator    = require('./lib/migrator');
+const config      = require('config');
 
 // Initialize the migrator with the options defined in config/[environment].js
 const migrator = new Migrator();
@@ -13,6 +14,25 @@ migraid
 process.on('unhandledRejection', (reason, p) => {
     console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason);
 });
+
+//*****************************************************************************
+//  Prevent Beta Scripts From Being Added to Production
+//****************************************************************************/
+const isBeta = process.cwd()
+    .includes('beta');
+console.log('Current working directoy: ' + process.cwd());
+if (isBeta) {
+    console.log('Detected BETA directory. This is from where the scripts will be loaded.');
+}
+console.log('NODE_ENV set to: ' + config.util.getEnv('NODE_ENV') + '. This determines the database the scripts will be executed in.');
+// If this environment is set to a production or development environment but the path contains the word "beta", throw an error. It is likely
+// that "npx migraid up" was called in the beta repository causing beta migration scripts to be added to a production database. Not good.
+if ((['production', 'development'].includes(config.util.getEnv('NODE_ENV')) || !config.util.getEnv('NODE_ENV')) && isBeta) {
+    throw new Error('Error: Detected a NODE_ENV of production or development while being in a beta directory. You should execute "# npm run migraid-beta" instead of "# npx migraid up".');
+}
+/////////////////////////////////////////////////////////////////////////////*/
+//  END Prevent Beta Scripts From Being Added to Production
+/////////////////////////////////////////////////////////////////////////////*/
 
 //*****************************************************************************
 //  Create Empty Migration File
